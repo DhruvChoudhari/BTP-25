7caae8c92d63a3593c586a8ae607c848ec0abe0a
Dhruv-IITP <dhruv_2101cs26@iitp.ac.in>
2024-08-30 16:52:02 +0530

    user activation header hero

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
   LoginPage,
-  SignupPage
+  SignupPage,
+  ActivationPage,
+  HomePage,
 } from "./routes/Routes";
-
-import { useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// import axios from "axios";
+// import { server } from "./server";
+import Store from "./redux/store";
+import { loadUser } from "./redux/actions/user";
 
 const App = () => {
+  useEffect(() => {
+    Store.dispatch(loadUser());
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignupPage />} />
+        <Route
+          path="/activation/:activation_token"
+          element={<ActivationPage />}
+        />
       </Routes>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </BrowserRouter>
   );
 };
