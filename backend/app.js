4a1da281c4d9f5854c3898e7f11f47e9b20cc5d7
Dhruv-IITP <dhruv_2101cs26@iitp.ac.in>
2024-08-30 16:52:02 +0530

    user activation header hero

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const ErrorHandler = require("./utils/ErrorHandler");
+// const ErrorHandler = require("./utils/ErrorHandler");
+const ErrorHandler = require("./middleware/error");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
@@ -7,8 +8,11 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-app.use("/", express.static("uploads"));
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials:true,
+}));
+app.use("/", express.static("../uploads"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 // config
